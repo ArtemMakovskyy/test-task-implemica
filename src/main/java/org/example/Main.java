4086cd10455e7f367fd4914b6fadd3ee40bd74bd
Artem Makovskyy <photomak79@gmail.com>
2024-11-28 09:37:36 +0200

    Implemented third task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1FindCorrectNumberBracketsExpressions();
+//        task1FindCorrectNumberBracketsExpressions();
+//        task3CalculateFactorialThenCalculateSumOfDigitsOfNumberOfFactorial(100);
     }
 
     /**
@@ -33,6 +34,31 @@ public class Main {
     }
 
     /**
+     *      Find the sum of the digits in the number 100! (i.e. 100 factorial)         {Correct answer: 648}
+     */
+    public static void task3CalculateFactorialThenCalculateSumOfDigitsOfNumberOfFactorial(int n) {
+        System.out.println("\nTask 3\nSum fo digits of factorial of number " + n + " is: " +
+                calculateSumOfDigitsOfNumber(
+                        calculateFactorial(n)
+                )
+        );
+    }
+
+    /**
+     * Calculates the sum of the digits in a given number.
+     *
+     * @param number The number to calculate the sum of digits for
+     * @return The sum of digits of the number
+     */
+    private static int calculateSumOfDigitsOfNumber(BigInteger number) {
+        // Convert the number to a string and calculate the sum of its digits
+        return number.toString()
+                .chars() // Get the stream of characters
+                .map(digit -> digit - '0') // Convert char to its numeric value
+                .sum(); // Sum up the numeric values
+    }
+
+    /**
      * Calculates the nth Catalan number using the formula:
      * C_n = (2n)! / ((n + 1)! * n!)
      *
@@ -52,8 +78,8 @@ public class Main {
      * @return The factorial of n
      */
     private static BigInteger calculateFactorial(int n) {
-        if (n == 0) {
-            return BigInteger.ZERO;
+        if (n == 0 || n == 1) {
+            return BigInteger.ONE;
         }
         BigInteger factorial = BigInteger.ONE;
         for (int i = 2; i <= n; i++) {
